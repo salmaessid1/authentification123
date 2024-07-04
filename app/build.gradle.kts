48a59a1e5ff33d2ce5f4e290e0008064f31de95f
Votre Nom <votre.email@example.com>
2024-07-05 00:35:45 +0200

    feat: done sign in (ui)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     alias(libs.plugins.android.application)
+    alias(libs.plugins.google.gms.google.services)
 }
 
 android {
@@ -34,10 +35,11 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+    implementation(libs.firebase.auth)
+    implementation(libs.firebase.database)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
-
 
     implementation(platform("com.google.firebase:firebase-bom:33.1.1"))
 }
--- app/src/main/java/com/example/authentification123/Sign_UpActivity.java
+++ app/src/main/java/com/example/authentification123/Sign_UpActivity.java
@@ -1,6 +1,8 @@
 package com.example.authentification123;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +12,18 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class Sign_UpActivity extends AppCompatActivity {
 
+    private TextView gotoSignIn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_sign_up);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        gotoSignIn = findViewById(R.id.gotoSignIn);
+        gotoSignIn.setOnClickListener(v ->{
+
+            startActivity(new Intent(Sign_UpActivity.this,Sign_InActivity.class));
         });
+
     }
 }
